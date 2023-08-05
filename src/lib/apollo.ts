191c7df35e786957ab567aff2c712f4b671d91aa
Rafael-jooj <rafaelrst15@gmail.com>
2023-08-05 18:58:21 -0300

    adicionando envs

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.hygraph.com/v2/clkso6xk30bhf01rsc5dadlw7/master',
+    uri: process.env.NEXT_PUBLIC_API_URL,
     cache: new InMemoryCache()
 })
